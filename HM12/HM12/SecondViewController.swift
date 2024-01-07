7023a2459aa5e53c3548edb6ae647b4bf193b1bd
EvgenSakun <evgen.sakun20@gmail.com>
2024-01-07 19:18:29 +0300

    Start

---
+++ HM12/HM12/SecondViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SecondViewController.swift
+//  HM12
+//
+//  Created by Evgeny Sakun on 7.01.24.
+//
+
+import Foundation
---
+++ HM12/HM12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM12
+//
+//  Created by Evgeny Sakun on 7.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
